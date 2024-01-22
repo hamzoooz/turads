86095332a2b4ddced218a469b5fddcafdf85d1fe
hamzoooz <hamzoooz123@gmail.com>
2024-01-22 21:08:18 +0300

    chnage static

--- themezoz/settings.py
+++ themezoz/settings.py
@@ -244,7 +244,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
